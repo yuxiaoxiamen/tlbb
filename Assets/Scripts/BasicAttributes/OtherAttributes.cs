1218a9238a5f6726e4bb35d06d2c7319dc73db04
git-ymk <45387957+git-ymk@users.noreply.github.com>
2020-05-03 16:45:55 +0800

    ymk

--- Assets/Scripts/BasicAttributes/OtherAttributes.cs
+++ Assets/Scripts/BasicAttributes/OtherAttributes.cs
@@ -14,17 +14,17 @@ public class OtherAttributes : MonoBehaviour
         switch(y.name)
         {
             case "ShengMing_Current":
-                percent = (float)GlobalData.Persons[characterId].BaseData.HP / 3000;
+                percent = (float)GlobalData.Persons[characterId].BaseData.HP / 3000;   //3000：最大生命
                 y.localScale = new Vector3(y.localScale.x * percent, y.localScale.y, y.localScale.z);
                 y.localPosition = new Vector3(3*(percent - 1) / 2, y.localPosition.y, y.localPosition.z);
                 break;
             case "NeiLi_Current":
-                percent = (float)GlobalData.Persons[characterId].BaseData.MP / 3000;
+                percent = (float)GlobalData.Persons[characterId].BaseData.MP / 3000;    //3000：最大内力
                 y.localScale = new Vector3(y.localScale.x * percent, y.localScale.y, y.localScale.z);
                 y.localPosition = new Vector3(3*(percent - 1) / 2, y.localPosition.y, y.localPosition.z);
                 break;
             case "Baoshi_Current":
-                percent = (float)GlobalData.Persons[characterId].BaseData.Energy / 100;
+                percent = (float)GlobalData.Persons[characterId].BaseData.Energy / 100;     //100：最大饱食
                 y.localScale = new Vector3(y.localScale.x * percent, y.localScale.y, y.localScale.z);
                 y.localPosition = new Vector3(3*(percent - 1) / 2, y.localPosition.y, y.localPosition.z);
                 break;
